3059a64f5ddeeec52656510f7ccecbf6c96a71a3
Henrry Salinas <es.henrry@gmail.com>
2016-06-02 16:47:46 -0400

    local changes to framework

--- Framework7/test-to-delete.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../Framework7/src/helpers/rest_client/api_rest_client'
-require_relative '../Framework7/src/requests/me_details_get'
-require_relative '../Framework7/src/requests/account/account_get'
-p client = ApiRestClient.new
-
-#p MeDetailsGet.validate_me_details(client)
-p AccountGet.validate_endpoint(client,'accounts','887587')
-
